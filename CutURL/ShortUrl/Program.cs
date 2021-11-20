82c184afea40c921d72b93485860eaa97cedd78d
Marik <marinahramzova2001@yandex.ru>
2021-11-20 03:26:45 +0300

    Fix pathCurrentDir

--- CutURL/ShortUrl/Program.cs
+++ CutURL/ShortUrl/Program.cs
@@ -8,8 +8,8 @@ namespace ShortUrl
         static void Main(string[] args)
         {
             var cutUrl = new CutUrl.Domain.CutUrl();
-            var getCurrent = Directory.GetCurrentDirectory();
-            var getUrlOnFile = cutUrl.GetURLOnFile(Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(getCurrent))) + @"\URL.txt");
+            var getCurrent = Path.GetDirectoryName(Directory.GetCurrentDirectory());
+            var getUrlOnFile = cutUrl.GetURLOnFile(Path.GetDirectoryName(Path.GetDirectoryName(getCurrent)) + @"\URL.txt");
             var shortUrl = cutUrl.GetShortURL(getUrlOnFile);
             for (int i = 0; i < shortUrl.Length; i++)
                 Console.WriteLine($"{getUrlOnFile[i]} => {shortUrl[i]}");
