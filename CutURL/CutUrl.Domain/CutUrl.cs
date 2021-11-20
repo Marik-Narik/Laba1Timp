b70329396e78bed617914a8df4ea0b68b1c0e145
Marik <marinahramzova2001@yandex.ru>
2021-11-20 03:16:16 +0300

    First

---
+++ CutURL/CutUrl.Domain/CutUrl.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace CutUrl.Domain
+{
+    public class CutUrl
+    {
+        public string[] GetShortURL(string[] longURL)
+        {
+            try
+            {
+                string[] shortURL = new string[longURL.Length];
+                for (int i = 0; i < longURL.Length; i++)
+                {
+                    using (WebClient wc = new WebClient())
+                    {
+                        shortURL[i] = wc.DownloadString("https://clck.ru/--?url=" + longURL[i]);
+                    }
+                }
+                return shortURL;
+            }
+            catch (WebException wex)
+            {
+                Console.WriteLine(wex.InnerException.Message);
+                return Array.Empty<String>();
+            }
+        }
+        public string[] GetURLOnFile(string filename) => File.ReadAllLines(filename);
+    }
+}
---
+++ CutURL/ShortUrl/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace ShortUrl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cutUrl = new CutUrl.Domain.CutUrl();
+            var getCurrent = Directory.GetCurrentDirectory();
+            var getUrlOnFile = cutUrl.GetURLOnFile(Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(getCurrent))) + @"\URL.txt");
+            var shortUrl = cutUrl.GetShortURL(getUrlOnFile);
+            for (int i = 0; i < shortUrl.Length; i++)
+            {
+                Console.WriteLine($"{getUrlOnFile[i]} => {shortUrl[i]}");
+            }
+        }
+    }
+}
